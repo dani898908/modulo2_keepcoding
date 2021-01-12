263c289005765051ae0fc9fbd80d599981880888
daniela <danielabonillac@hotmail.com>
2021-01-12 09:07:04 +0100

    pg 2

--- funcionesLambda.py
+++
@@ -1,3 +0,0 @@
-from funciones1nivel import sumaTodos
-
-print(sumaTodos(3, lambda x:x*3))
--- suma.py
+++
@@ -1,7 +0,0 @@
-def sumaTodo(limitTo):
-    resultado= 0
-    for i in range(0, limitTo+1):
-        resultado+= i
-
-    return resultado
-print(sumaTodo(100))
